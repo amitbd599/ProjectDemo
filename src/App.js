cc656204a0daf1d3b2da7ef2b3cd833611e5a7c9
amitbd599 <amitbd599@gmail.com>
2022-04-28 18:23:49 +0600

    add image and bgImg for looping

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bgImg bgImg-1">
     <h2 className="pt-200 pt-lg-150 pt-md-150 pt-sm-15 font-60 font-md-30 font-sm-15">Hello</h2>
+    <img src="" alt="" />
     </div>
   );
 }
